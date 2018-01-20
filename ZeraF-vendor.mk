e6818815f4716ea3fed5858336467c82c1d4040f
cerg2010cerg2010 <cerg2010cerg2010@mail.ru>
2018-01-20 19:51:10 +0300

    Camera fix, change name

---
+++ ZeraF-vendor.mk
@@ -0,0 +1,9 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Highscreen/ZeraF/proprietary/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Highscreen/ZeraF/proprietary/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Highscreen/ZeraF/proprietary/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Highscreen/ZeraF/proprietary/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Highscreen/ZeraF/proprietary/vendor,system/vendor)
